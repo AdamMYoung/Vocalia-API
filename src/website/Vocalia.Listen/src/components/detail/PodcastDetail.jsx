dc115de32697a631c6f28e56c26f42e6fc318f76
Adam Young <adamyoungg97@gmail.com>
2019-02-02 19:50:16 +0000

    Added initial dialog box to nav

--- src/website/Vocalia.Listen/src/components/detail/PodcastDetail.jsx
+++ src/website/Vocalia.Listen/src/components/detail/PodcastDetail.jsx
@@ -1,18 +1,46 @@
 import React, { Component } from "react";
 import Dialog from "@material-ui/core/Dialog";
 import { withMobileDialog } from "@material-ui/core";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Button from "@material-ui/core/Button";
+let Parser = require("rss-parser");
 
 class PodcastDetail extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      feed: null
+    };
+  }
+
   render() {
-    return (
-      <Dialog
-        open={this.props.open}
-        onClose={this.props.onClose}
-        fullScreen={this.props.fullScreen}
-      >
-        <p>{this.props.rssUrl}</p>
-      </Dialog>
-    );
+    function loadDialog(props, state) {
+      return (
+        <Dialog
+          open={props.open}
+          onClose={props.onClose}
+          fullScreen={props.fullScreen}
+        >
+          <DialogTitle id="responsive-dialog-title">Title</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              This is where the description will go.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={props.onClose} color="primary" autoFocus>
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
+      );
+    }
+
+    return loadDialog(this.props, this.state);
   }
 }
 
