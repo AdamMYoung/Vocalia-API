efdffaad1ec22eb810772b8e5573278e81d841e2
Adam Young <adamyoungg97@gmail.com>
2019-02-03 01:27:54 +0000

    Added dynamic loading of podcast information

--- src/website/Vocalia.Listen/src/components/detail/PodcastDetail.jsx
+++ src/website/Vocalia.Listen/src/components/detail/PodcastDetail.jsx
@@ -6,41 +6,59 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Button from "@material-ui/core/Button";
+import Fade from "@material-ui/core/Fade";
+
 let Parser = require("rss-parser");
+
+const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
 
 class PodcastDetail extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      feed: null
+      feed: {}
     };
   }
 
-  render() {
-    function loadDialog(props, state) {
-      return (
-        <Dialog
-          open={props.open}
-          onClose={props.onClose}
-          fullScreen={props.fullScreen}
-        >
-          <DialogTitle id="responsive-dialog-title">Title</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              This is where the description will go.
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={props.onClose} color="primary" autoFocus>
-              Close
-            </Button>
-          </DialogActions>
-        </Dialog>
-      );
-    }
+  componentWillReceiveProps(props) {
+    if (props.rssUrl !== null) {
+      let parser = new Parser();
+      this.setState({ loading: true, feed: {} });
 
-    return loadDialog(this.props, this.state);
+      (async () => {
+        let parsedFeed = await parser.parseURL(CORS_PROXY + props.rssUrl);
+        this.setState({ feed: parsedFeed, loading: false });
+      })();
+    }
+  }
+
+  render() {
+    const { feed, loading } = this.state;
+
+    return (
+      <Dialog
+        open={this.props.open}
+        onClose={this.props.onClose}
+        fullScreen={this.props.fullScreen}
+      >
+        {!loading && (
+          <Fade in={!loading}>
+            <div>
+              <DialogTitle>{feed.title}</DialogTitle>
+              <DialogContent>
+                <DialogContentText>{feed.description}</DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={this.props.onClose} color="primary" autoFocus>
+                  Close
+                </Button>
+              </DialogActions>
+            </div>
+          </Fade>
+        )}
+      </Dialog>
+    );
   }
 }
 
