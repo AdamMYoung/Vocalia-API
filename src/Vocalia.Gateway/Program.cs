3496dcb61fc28ec69e8d33c47f158980dcfc4b9c
Adam Young <adamyoungg97@gmail.com>
2019-02-14 20:03:08 +0000

    Removed CORS from gateway and updated ocelot.json

--- src/Vocalia.Gateway/Program.cs
+++ src/Vocalia.Gateway/Program.cs
@@ -32,14 +32,7 @@ namespace Vocalia.Gateway
                     .AddEnvironmentVariables();
             })
             .ConfigureServices(s => {
-                s.AddCors(options =>
-                {
-                    options.AddPolicy("CorsPolicy",
-                    builder => builder.AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials());
-                });
+               
                 s.AddOcelot();
             })
             .ConfigureLogging((hostingContext, logging) =>
@@ -49,7 +42,7 @@ namespace Vocalia.Gateway
             .UseIISIntegration()
             .Configure(app =>
             {
-                app.UseCors("CorsPolicy");
+       
                 app.UseOcelot().Wait();
             })
             .Build()
